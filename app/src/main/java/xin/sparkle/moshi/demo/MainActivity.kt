a6300c172e831c14b3d94a01100bba82bee8dfb8
xiaowei.lei <xiaowei.lei@thoughtworks.com>
2021-10-23 21:07:40 +0800

    Init project.

---
+++ app/src/main/java/xin/sparkle/moshi/demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package xin.sparkle.moshi.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
